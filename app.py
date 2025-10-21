1558b08e6ac1651515fc4fde34ab7c0b91a8a40a
Premnat30 <n.premnatarajan@gmail.com>
2025-10-22 00:56:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-mport os
+import os
 from flask import Flask, render_template, request, redirect, url_for, session, flash, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
